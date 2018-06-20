24324d9bb9925275a7009c9da8aed3d5924b6e7e
Wang Chunfei <1041875526@qq.com>
2018-06-20 22:27:43 +0800

    init commit

---
+++ src/components/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu-item index="1" class="logo">OnlineJudge</el-menu-item>
+    <el-menu-item index="2">
+      <i class="el-icon-tickets"></i>
+      Problem
+    </el-menu-item>
+    <el-menu-item index="3">Contests</el-menu-item>
+    <el-menu-item index="4">Status</el-menu-item>
+    <el-menu-item index="5">Rank</el-menu-item>
+    <el-menu-item index="6">Blog</el-menu-item>
+    <div v-if="!isSignIn">
+      <el-button type="primary" style="float: right;margin-top: 8px">Sign in</el-button>
+    </div>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      isSignIn: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .logo {
+    margin-left: 2%;
+    margin-right: 2%;
+    font-size: 20px;
+    float: left;
+    line-height: 60px;
+  }
+</style>
---
+++ src/store/users.js
@@ -0,0 +1,5 @@
+// 用户状态
+const state = {
+  isSignIn: 0,
+  username: null
+}
