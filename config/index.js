0f7f6fc734b5b07153dc7e19eadc0c9922cb9930
Wang Chunfei <1041875526@qq.com>
2018-07-06 13:59:45 +0800

    fix dev port

--- config/index.js
+++ config/index.js
@@ -12,18 +12,18 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api':{
-        target:'http://106.15.183.211:8080',
+        target:'http://localhost:8080',
         changeOrigin:true
       },
       '/filter_api':{
-        target:'http://106.15.183.211:8080',
+        target:'http://localhost:8080',
         changeOrigin:true
       },
     },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
