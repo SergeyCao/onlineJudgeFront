89498dd8aed13494571ab78ea870ed639992f3c9
pushmyandy <1805311490@qq.com>
2018-08-02 10:26:56 +0800

    change this sign box view

--- src/components/oj/user/SignIn.vue
+++ src/components/oj/user/SignIn.vue
@@ -1,31 +1,35 @@
 <template>
-  <el-row>
-    <el-col :span="8" :offset="8">
-      <el-form :model="account" status-icon :rules="rule" ref="account" label-width="100px" class="demo-ruleForm">
-        <el-form-item label="用户名/邮箱" prop="username">
-          <el-input type="text" v-model="account.username" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input type="password" v-model="account.password" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-checkbox v-model="account.isRemember">七天内自动登录</el-checkbox>
-        </el-form-item>
-        <el-form-item>
-          <span style="float: left">
-            <router-link :to="{name:'register'}" style="text-decoration: none;color:blue">没有账号？立即注册</router-link>
-          </span>
-          <span style="float: right">
-            <router-link :to="{name:'forgetPwd'}" style="text-decoration: none;color:blue">忘记密码</router-link>
-          </span>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('account')">提交</el-button>
-          <el-button @click="resetForm('account')">重置</el-button>
-        </el-form-item>
-      </el-form>
-    </el-col>
-  </el-row>
+  <div class="login">
+    <el-row>
+      <el-col>
+        <el-form :model="account" status-icon :rules="rule" ref="account" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="用户名/邮箱" prop="username">
+            <el-input type="text" v-model="account.username" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input type="password" v-model="account.password" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox v-model="account.isRemember">七天内自动登录</el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <span style="float: left;color: cornflowerblue;">
+              <router-link :to="{name:'register'}" style="text-decoration: none;">没有账号？立即注册</router-link>
+            </span>
+            <span style="float: right;color: cornflowerblue;">
+              <router-link :to="{name:'forgetPwd'}" style="text-decoration: none;">忘记密码</router-link>
+            </span>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('account')"
+                       style="float: left">提交</el-button>
+            <el-button @click="resetForm('account')"
+                       style="float: right">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -101,29 +105,17 @@ export default {
 }
 </script>
 
-<style scoped>
-  .el-row {
-    margin-bottom: 20px;
-    margin-top: 20px;
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
+<style scoped lang="stylus">
+  .login
+    position relative
+    border-radius 10px
+    background-image url("../../../assets/imgs/white.jpg")
+    box-sizing border-box
+    padding 20px
+    width 40%
+    margin 10px auto
+    span a
+      color cornflowerblue
+      &:hover
+        color lightgreen
 </style>
